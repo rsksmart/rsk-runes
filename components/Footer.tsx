447f0682ef01d2b0e7e23396f906d27c48296336
chrisarevalo11 <christian.arevalo.fonseca@gmail.com>
2024-05-29 03:33:49 -0500

    📱 Worked on responsive footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { Discord, Github, Twitter } from '@/components/icons'
 
 export default function Footer(): JSX.Element {
   return (
-    <footer className="flex items-center justify-between w-full py-3 px-5">
+    <footer className="flex lg:items-center items-start flex-col gap-6 lg:flex-row justify-between w-full py-3 px-5">
       <div>
         <p>
           Built by <span className="text-xl font-bold">RootstockLabs</span>
